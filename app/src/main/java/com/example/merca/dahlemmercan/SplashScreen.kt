e06080da7015f28b0e1ea565c4aa5b0cc34c691e
MercanBrandon <mercan.brandon@outlook.fr>
2019-01-11 13:21:47 +0100

    commit B 3

--- app/src/main/java/com/example/merca/dahlemmercan/SplashScreen.kt
+++ app/src/main/java/com/example/merca/dahlemmercan/SplashScreen.kt
@@ -1,12 +1,23 @@
 package com.example.merca.dahlemmercan
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 
 class SplashScreen : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            val intent: Intent = if (Preferences.userName.isEmpty() && Preferences.userPwd.isEmpty())
+                Intent(applicationContext, LoginActivity::class.java)
+            else
+                Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
