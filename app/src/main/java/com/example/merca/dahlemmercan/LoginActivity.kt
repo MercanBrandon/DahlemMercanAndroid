d21433d7304ced21a59c5c88494d3a2372987101
Darman <dahlem.romain@gmail.com>
2019-01-11 16:08:04 +0100

    Abandon de follower désolé monsieur

--- app/src/main/java/com/example/merca/dahlemmercan/LoginActivity.kt
+++ app/src/main/java/com/example/merca/dahlemmercan/LoginActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import com.chibatching.kotpref.Kotpref
 import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.toast
 
 class LoginActivity : AppCompatActivity() {
 
@@ -17,12 +18,15 @@ class LoginActivity : AppCompatActivity() {
             btnConnexion.setOnClickListener {
                 var userName : String = loginLogin.text.toString()
                 var userPwd : String = loginPassword.text.toString()
+                if(userPwd.isNotEmpty() && userName.isNotEmpty()) {
+                    Preferences.userName = userName
+                    Preferences.userPwd = userPwd
 
-                Preferences.userName = userName
-                Preferences.userPwd = userPwd
-
-                val intent = Intent(applicationContext, MainActivity::class.java)
-                startActivity(intent)
+                    val intent = Intent(applicationContext, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    toast("Veuillez saisir le login et le mot de passe!")
+                }
             }
     }
 }
--- app/src/main/java/com/example/merca/dahlemmercan/fragment/FollowersFragment.kt
+++ app/src/main/java/com/example/merca/dahlemmercan/fragment/FollowersFragment.kt
@@ -1,19 +1,16 @@
 package com.example.merca.dahlemmercan.fragment
 
-
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-import com.example.merca.dahlemmercan.R
 import com.example.merca.dahlemmercan.adapter.GitHubAdapter
 import com.example.merca.dahlemmercan.api.GitHubInterface
 import com.example.merca.dahlemmercan.api.RetrofitClient
-import com.example.merca.dahlemmercan.models.Follower
 import com.example.merca.dahlemmercan.models.ListFollower
+
+import com.example.merca.dahlemmercan.R
 import kotlinx.android.synthetic.main.fragment_followers.*
 import org.jetbrains.anko.toast
 import retrofit2.Call
@@ -29,10 +26,9 @@ class FollowersFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_followers, container, false)
     }
 
+/*
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        recyclerFollower.layoutManager = LinearLayoutManager(context)
 
         val client = RetrofitClient().getClient().create(GitHubInterface::class.java)
         client.getAllFollowers().enqueue(object : retrofit2.Callback<ListFollower>{
@@ -52,5 +48,5 @@ class FollowersFragment : Fragment() {
             }
 
         })
-    }
+    }*/
 }
